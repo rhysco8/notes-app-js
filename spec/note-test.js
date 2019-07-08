b1703b5a009932e0d43de61dcd1672f57222da8d
Student <kaczanowskamonika@gmail.com>
2019-07-08 17:10:39 +0100

    assert extracted

--- spec/note-test.js
+++ spec/note-test.js
@@ -8,14 +8,6 @@
 //   };
 //   testNoteInitialization();
 // })(this);
-  var assert = {
-    isTrue: function(assertionToCheck) {
-      if (!assertionToCheck) {
-        throw new Error("Assertion failed: " + assertionToCheck + "is not truthy");
-      }
-    }
-  }
-
 
   function testNoteInitialization() {
     var note= new Note("test note");
